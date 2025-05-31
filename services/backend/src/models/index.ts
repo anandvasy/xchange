8d6db1b84aaeb9108e61d47e1eeb3baf3e8bd1e4
Anand Vasudevan <anandvas@yahooinc.com>
2025-05-30 20:53:33 -0700

    Remove web app and unused services

--- services/backend/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { User } from './User';
-export { Community } from './Community';
-export { CommunityMember } from './CommunityMember';
-export { Post } from './Post';
-export { Comment } from './Comment'; 
